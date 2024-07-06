73783b2f9bb533840b517b8905dc7aae1338e09b
Alexey Tyurin <alexsei89tyrin@gmail.com>
2024-07-06 23:05:03 +0400

    Add FSD layout

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import './reset.scss'
+import './main.scss'
+import { Button } from '@/shared/ui/Button'
+
+function App() {
+	return (
+		<>
+			<Button className='test'>Кнопка</Button>
+		</>
+	)
+}
+
+export default App
